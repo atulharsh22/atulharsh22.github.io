84125d84687079b93fb563779be0edabca42e081
atulharsh22 <atulharsh22@gmail.com>
2023-02-19 21:57:37 +0530

    added portfolio

---
+++ home.js
@@ -0,0 +1,45 @@
+document.querySelector("#contact-github").addEventListener('click',function(){
+    window.open("https://github.com/atulharsh22");
+});
+
+document.querySelector("#contact-linkedin").addEventListener('click',function(){
+    window.open("https://www.linkedin.com/in/atul-harsh/");
+});
+
+
+document.querySelector("#project-1-deployed").addEventListener('click',function(){
+    window.open("https://fastidious-fudge-60d814.netlify.app/");
+});
+
+document.querySelector("#project-1-github").addEventListener('click',function(){
+    window.open("https://github.com/gulam160/beneficial-spiders-5753");
+});
+
+document.querySelector("#project-2-deployed").addEventListener('click',function(){
+    window.open("https://dashing-pie-f1ac87.netlify.app/");
+});
+
+document.querySelector("#project-2-github").addEventListener('click',function(){
+    window.open("https://github.com/skshm2000/SendInBlue-Online-Marketing-Services");
+});
+
+document.querySelector("#project-3-deployed").addEventListener('click',function(){
+    window.open("https://whimsical-lebkuchen-84f0b3.netlify.app/");
+});
+
+document.querySelector("#project-3-github").addEventListener('click',function(){
+    window.open("https://github.com/atulharsh22/youtube-clone");
+});
+
+document.querySelector("#project-4-deployed").addEventListener('click',function(){
+    window.open("https://exquisite-stroopwafel-1b750a.netlify.app/");
+});
+
+document.querySelector("#project-4-github").addEventListener('click',function(){
+    window.open("https://github.com/pankajmorecreativity/plastic-scent-4061");
+});
+
+
+document.querySelector("#resume-link-2").addEventListener('click',function(){
+    window.location.href="Atul_Harsh_resume.pdf";
+})
